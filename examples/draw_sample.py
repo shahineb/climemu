d6360b5e7b3223ba57cb47f4a593353c5a907c17
shahineb <shahineb@mit.edu>
2025-09-09 13:57:55 -0400

    push to save

---
+++ examples/draw_sample.py
@@ -0,0 +1,92 @@
+from functools import partial
+import xarray as xr
+import jax.numpy as jnp
+import jax.random as jr
+import equinox as eqx
+from src.diffusion import HealPIXUNet, ContinuousVESchedule
+from experiments.mpi.config import Config
+import .utils as utils
+
+
+
+def build_nn(config):
+    # Load edges
+    edges_to_healpix, edges_to_latlon = utils.load_edges(config.data.edges_path)
+
+    # Initialize the neural network
+    nn = HealPIXUNet(input_size=config.model.input_size,
+                     nside=config.model.nside,
+                     enc_filters=list(config.model.enc_filters),
+                     dec_filters=list(config.model.dec_filters),
+                     out_channels=config.model.out_channels,
+                     temb_dim=config.model.temb_dim,
+                     healpix_emb_dim=config.model.healpix_emb_dim,
+                     edges_to_healpix=edges_to_healpix,
+                     edges_to_latlon=edges_to_latlon)
+    nn = eqx.tree_deserialise_leaves(config.training.model_filename, nn)
+    utils.print_parameter_count(nn)
+    return nn
+
+def build_schedule(config, σmax):
+    σmax = utils.load_σmax(config.data.sigma_max_path)
+    schedule = ContinuousVESchedule(config.schedule.sigma_min, σmax)
+    return schedule
+    
+
+def build_generative_model(config, n_samples, n_steps):
+    nn = build_nn(config)
+    schedule = build_schedule(config)
+    μ, σ = utils.load_normalization(config.data.norm_stats_path)
+    generative_model = partial(utils.draw_samples_single,
+                               model=nn,
+                               schedule=schedule,
+                               n_samples=n_samples,
+                               n_steps=n_steps,
+                               μ=μ, σ=σ)
+    return generative_model
+
+def build_emulator(config, n_samples=1, n_steps=30):
+    # Load generative model and pattern scaling coefficients
+    generative_model = build_generative_model(config, n_samples, n_steps)
+    β = utils.load_β(config.data.β_path)
+
+    # dry run to compile
+    nlat, nlon = config.model.input_size[1], config.model.input_size[2]
+    _ = generative_model(pattern=jnp.zeros((nlat, nlon)), key=jr.PRNGKey(0))
+
+    def emulator(ΔT, month, seed):
+        pattern = β[month - 1, :, 1] * ΔT + β[month - 1, :, 0]
+        pattern = pattern.reshape((nlat, nlon))
+        key = jr.PRNGKey(seed)
+        samples = generative_model(pattern=pattern, key=key)
+        return samples
+    return emulator
+
+
+if __name__ == "__main__":
+    config = Config()
+    emulator = build_emulator(config, n_samples=1, n_steps=30)
+
+    ΔT = 2.0  # Temperature anomaly in °C
+    month = 1  # January
+    seed = 0  # Random seed for reproducibility
+    samples = emulator(ΔT=ΔT, month=month, seed=seed)
+
+
+    # Save samples to a NetCDF file
+    varnames = config.data.variables
+    nlat, nlon = config.model.input_size[1], config.model.input_size[2]
+    ds = xr.Dataset(
+        {
+            var: (('member', 'lat', 'lon'), samples[:, i, :, :])
+            for i, var in enumerate(config.data.variables)
+        },
+        coords={
+            'member': jnp.arange(len(samples)) + 1,
+            'lat': jnp.arange(nlat),
+            'lon': jnp.arange(nlon),
+        }
+    )
+    ds.to_netcdf("./emulator_samples.nc")
+    
+
